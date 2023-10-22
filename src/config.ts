755867c8a01105cdd2f2c1c11236ae47232818c6
nyxb <contact@nyxb.email>
2023-10-22 17:15:30 +0200

    ✨ feat(cli.ts, config.ts): add new CLI and configuration files
    📝 docs(cli.ts): add command line interface for the application with subcommands for default and GitHub operations
    
    📝 docs(config.ts): add configuration file for managing changelog settings, including default settings and environment variable support

---
+++ src/config.ts
@@ -0,0 +1,122 @@
+import { resolve } from 'node:path'
+import { loadConfig, setupDotenv } from 'c12'
+import { getCurrentGitRef, getLastGitTag } from './git'
+import { getRepoConfig, resolveRepoConfig } from './repo'
+import type { SemverBumpType } from './semver'
+import type { RepoConfig, RepoProvider } from './repo'
+
+export interface ChangelogConfig {
+   cwd: string
+   types: Record<string, { title: string; semver?: SemverBumpType }>
+   scopeMap: Record<string, string>
+   repo?: RepoConfig | string
+   tokens: Partial<Record<RepoProvider, string>>
+   from: string
+   to: string
+   newVersion?: string
+   output: string | boolean
+   publish: {
+      args?: string[]
+      tag?: string
+      private?: boolean
+   }
+   templates: {
+      commitMessage?: string
+      tagMessage?: string
+      tagBody?: string
+   }
+   excludeAuthors: string[]
+}
+
+export type ResolvedChangelogConfig = Omit<ChangelogConfig, 'repo'> & {
+   repo: RepoConfig
+}
+
+const defaultOutput = 'CHANGELOG.md'
+function getDefaultConfig() {
+   return <ChangelogConfig>{
+      types: {
+         feat: { title: '🚀 Enhancements', semver: 'minor' },
+         perf: { title: '🔥 Performance', semver: 'patch' },
+         fix: { title: '🩹 Fixes', semver: 'patch' },
+         refactor: { title: '💅 Refactors', semver: 'patch' },
+         docs: { title: '📖 Documentation', semver: 'patch' },
+         build: { title: '📦 Build', semver: 'patch' },
+         types: { title: '🌊 Types', semver: 'patch' },
+         chore: { title: '🧹 Chore' },
+         examples: { title: '💡 Examples' },
+         test: { title: '✅ Tests' },
+         style: { title: '🎨 Styles' },
+         ci: { title: '🤖 CI' },
+      },
+      cwd: null,
+      from: '',
+      to: '',
+      output: defaultOutput,
+      scopeMap: {},
+      tokens: {
+         github:
+        process.env.CRAFTCHANGELOGS_TOKENS_GITHUB
+        || process.env.GITHUB_TOKEN
+        || process.env.GH_TOKEN,
+      },
+      publish: {
+         private: false,
+         tag: 'latest',
+         args: [],
+      },
+      templates: {
+         commitMessage: 'chore(release): v{{newVersion}}',
+         tagMessage: 'v{{newVersion}}',
+         tagBody: 'v{{newVersion}}',
+      },
+      excludeAuthors: [],
+   }
+}
+
+export async function loadChangelogConfig(
+   cwd: string,
+   overrides?: Partial<ChangelogConfig>,
+): Promise<ResolvedChangelogConfig> {
+   await setupDotenv({ cwd })
+   const defaults = getDefaultConfig()
+   const { config } = await loadConfig<ChangelogConfig>({
+      cwd,
+      name: 'changelog',
+      packageJson: true,
+      defaults,
+      overrides: {
+         cwd,
+         ...(overrides as ChangelogConfig),
+      },
+   })
+
+   return await resolveChangelogConfig(config, cwd)
+}
+
+export async function resolveChangelogConfig(
+   config: ChangelogConfig,
+   cwd: string,
+) {
+   if (!config.from)
+      config.from = await getLastGitTag()
+
+   if (!config.to)
+      config.to = await getCurrentGitRef()
+
+   if (config.output) {
+      config.output
+      = config.output === true ? defaultOutput : resolve(cwd, config.output)
+   }
+   else {
+      config.output = false
+   }
+
+   if (!config.repo)
+      config.repo = await resolveRepoConfig(cwd)
+
+   if (typeof config.repo === 'string')
+      config.repo = getRepoConfig(config.repo)
+
+   return config as ResolvedChangelogConfig
+}
